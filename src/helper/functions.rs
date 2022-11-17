0f3783a89a78bb16bf57f662e682cf78609bba56
Lorenzo Pantano <l.pantano97@gmail.com>
2022-11-17 22:23:48 +0100

    cover working

--- src/helper/functions.rs
+++ src/helper/functions.rs
@@ -1,7 +1,25 @@
-use std::path::PathBuf;
-use native_dialog::{FileDialog};
+use native_dialog::FileDialog;
+use std::{io::Read, path::PathBuf};
 
+/**
+ * Open a file dialog and return the path to the selected file.
+ */
 pub fn open_native_dialog() -> Option<PathBuf> {
-    let path = FileDialog::new().set_location("~/Desktop").add_filter("EPub", &["epub"]).show_open_single_file().unwrap();
+    let path = FileDialog::new()
+        .set_location("~/Desktop")
+        .add_filter("EPub", &["epub"])
+        .show_open_single_file()
+        .unwrap();
     path
+}
+
+/**
+ * Convert a path into a file and then into a byte array [u8] and return it.
+ */
+pub fn path_to_bytes(path: PathBuf) -> Option<Vec<u8>> {
+    let file = std::fs::File::open(path).unwrap();
+    let mut bytes = Vec::new();
+    let mut reader = std::io::BufReader::new(file);
+    reader.read_to_end(&mut bytes).unwrap();
+    Some(bytes)
 }
--- src/view/home/ui.rs
+++ src/view/home/ui.rs
@@ -1,9 +1,12 @@
-use crate::helper::config::{ PADDING_LG, TITLE};
+use crate::helper::config::{COVER_PLACEHOLDER, PADDING_LG, TITLE};
 use crate::model::book::Book;
 use crate::model::library::Library;
 use crate::{AppState, APP_NAME};
-use druid::widget::{Button, FillStrat, Flex, Image, Label, List, MainAxisAlignment, Padding};
-use druid::{Insets, Widget, WidgetExt, LensExt, ImageBuf};
+use druid::widget::{
+    Button, FillStrat, Flex, Image, Label, List, MainAxisAlignment, Padding, Scroll, Svg,
+    ViewSwitcher,
+};
+use druid::{lens, ImageBuf, Insets, LensExt, Widget, WidgetExt};
 
 /** Notes on Data and Lens.
    Il tratto Lens permette di accedere ad una porzione di una struttura dati
@@ -18,8 +21,13 @@ use druid::{Insets, Widget, WidgetExt, LensExt, ImageBuf};
 /* Home ui builder */
 pub fn build_ui() -> impl Widget<AppState> {
     let header = header();
-    let books_list = List::new(book_item).lens(AppState::library.then(Library::books));  // Lens chaining
-    let layout = Flex::column().with_child(header).with_child(books_list);
+    let books_list = Scroll::new(List::new(book_item))
+        .vertical()
+        .lens(AppState::library.then(Library::books)); // Lens chaining
+    let layout = Flex::column()
+        .with_child(header)
+        .with_child(books_list)
+        .fix_height(500.0);
     Padding::new(
         Insets::new(PADDING_LG, PADDING_LG, PADDING_LG, PADDING_LG),
         layout,
@@ -29,7 +37,8 @@ pub fn build_ui() -> impl Widget<AppState> {
 /* Header section */
 fn header() -> impl Widget<AppState> {
     let header_label = Label::new(APP_NAME).with_font(TITLE);
-    let add_book_button = Button::new("Add book").on_click(|_, data: &mut AppState, _| data.add_book());
+    let add_book_button =
+        Button::new("Add book").on_click(|_, data: &mut AppState, _| data.add_book());
     let mut header = Flex::row()
         .with_child(header_label)
         .main_axis_alignment(MainAxisAlignment::SpaceBetween)
@@ -41,8 +50,18 @@ fn header() -> impl Widget<AppState> {
 /* Book item */
 fn book_item() -> impl Widget<Book> {
     let title = Label::raw().lens(Book::title);
-    let image_buf = ImageBuf::from_data(include_bytes!("../../library/covers/Notre-Dame-De-Paris.png")).unwrap();
-    // TODO: Come cazzo si fa
-    let image = Image::new(image_buf).fill_mode(FillStrat::Contain).fix_size(150.0, 250.0);
-    Flex::column().with_child(title).with_child(image)
+    let cover = Flex::row().with_child(ViewSwitcher::new(
+        |data: &Book, _env| data.get_image_buf().is_some(),
+        move |f, data, _env| {
+            if *f {
+                Box::new(
+                    Image::new(data.get_image_buf().as_ref().unwrap().as_ref().clone())
+                        .fix_size(100.0, 200.0),
+                )
+            } else {
+                Box::new(Svg::new(COVER_PLACEHOLDER.parse().unwrap()).fill_mode(FillStrat::Fill))
+            }
+        },
+    ));
+    Flex::column().with_child(title).with_child(cover)
 }
