b96a8e5270ff326cfb38d3933569181c7b685f0d
giacomo13 <giacomomaggio13@gmail.com>
2022-12-06 16:24:21 +0100

    implemented new book container

--- src/view/home/ui.rs
+++ src/view/home/ui.rs
@@ -44,11 +44,13 @@ pub fn build_ui() -> impl Widget<AppState> {
 /* Library View */
 fn library_view() -> impl Widget<AppState> {
     let header = header();
-    let book_list = Scroll::new(List::new(book_item))
+    let book_list = Flex::row().with_child(Scroll::new(List::new(book_item))
         .vertical()
-        .lens(AppState::library.then(Library::books)); // Lens chaining
+        .lens(AppState::library.then(Library::books))); // Lens chaining
 
-    let layout = Flex::row().with_child(header).with_child(book_list);
+    let mut layout = Flex::column();
+    layout.add_child(header);
+    layout.add_child(Flexbook_list);
 
     Padding::new(
         Insets::new(PADDING_LG, PADDING_LG, PADDING_LG, PADDING_LG),
@@ -59,6 +61,7 @@ fn library_view() -> impl Widget<AppState> {
 /* Book item */
 fn book_item() -> impl Widget<Book> {
     let title = Label::raw().lens(Book::title);
+    let author = Label::raw().lens(Book::author);
 
     let cover = Flex::row().with_child(ViewSwitcher::new(
         |data: &Book, _env| data.get_image_buf().is_some(),
@@ -78,10 +81,29 @@ fn book_item() -> impl Widget<Book> {
         println!("Opening book: {}", data.get_title());
         ctx.submit_command(OPEN_BOOK.with(data.clone()));
     });
-    Flex::row()
-        .with_child(title)
-        .with_child(cover)
-        .with_child(button)
+
+    let mut book_layout = Flex::row();
+
+    let mut col_details = Flex::column().cross_axis_alignment(CrossAxisAlignment::Start);
+    col_details.add_child(title);
+    col_details.add_spacer(10.0);
+    col_details.add_child(author);
+    col_details.add_child(button);
+    col_details.add_spacer(50.0);
+
+    book_layout.add_child(cover);
+    book_layout.add_spacer(10.0);
+    book_layout.add_child(col_details);
+
+    let container = Flex::column()
+        .with_child(book_layout)
+        .fix_size(340.0, 200.0)
+        .padding(2.0)
+        .border(Color::GRAY, 2_f64);
+
+    let controller_host = ControllerHost::new(container, Click::new(|_, _, _| println!("Click")));
+
+    return controller_host;
 }
 
 /* Header section */
@@ -102,84 +124,4 @@ fn header() -> impl Widget<AppState> {
     header.add_child(add_book_button);
 
     header
-}
-
-fn books_container() -> impl Widget<Library> {
-    let container = Flex::row().with_child(ViewSwitcher::new(
-        |data: &Library, _env| (data.get_length() > 0_f64), //Se ho libri in libreria
-        move |f, data, _env| {
-            if *f {
-                let cols = 3;
-                let rows = (data.get_length() / 3_f64).ceil() as i32;
-
-                let mut col = Flex::column().cross_axis_alignment(CrossAxisAlignment::Start);
-                let books = data.get_books_vec();
-                let mut component_vector = Vec::new(); //Vettore di componenti fatti con la funzione di gui
-
-                for book in books {
-                    component_vector.push(book_component(book));
-                }
-
-                for _ in 0..rows {
-                    let mut row = Flex::row();
-                    for _ in 0..cols {
-                        let component = component_vector.pop();
-                        match component {
-                            Some(x) => row = row.with_child(x),
-                            None => {}
-                        }
-                    }
-                    col = col.with_child(row);
-                }
-
-                Box::new(Scroll::new(col))
-            } else {
-                let label = Label::new("There are no books in the library!")
-                    .with_font(ALERT)
-                    .padding(Insets::new(0.0, 200.0, 0.0, 0.0));
-                Box::new(label)
-            }
-        },
-    ));
-
-    return container;
-}
-
-fn book_component(book: Book) -> impl Widget<Library> {
-    let title = Label::new(book.get_title().as_str());
-    let author = Label::new(book.get_author().as_str());
-
-    let mut book_layout = Flex::row();
-
-    //TODO: Gestire il caso di mancanza della copertina
-
-    let mut col_details = Flex::column().cross_axis_alignment(CrossAxisAlignment::Start);
-    col_details.add_child(title);
-    col_details.add_spacer(10.0);
-    col_details.add_child(author);
-    col_details.add_spacer(50.0);
-
-    match book.get_image_buf() {
-        Some(_) => {
-            let cover =
-                Image::new(book.get_image_buf().unwrap().as_ref().clone()).fix_size(100.0, 200.0);
-            book_layout.add_child(Flex::column().with_child(cover));
-        }
-        None => {
-            let cover = Svg::new(COVER_PLACEHOLDER.parse().unwrap()).fill_mode(FillStrat::Fill);
-            book_layout.add_child(Flex::column().with_child(cover))
-        }
-    }
-    book_layout.add_spacer(10.0);
-    book_layout.add_child(col_details);
-
-    let container = Flex::column()
-        .with_child(book_layout)
-        .fix_size(340.0, 200.0)
-        .padding(2.0)
-        .border(Color::GRAY, 2_f64);
-
-    let controller_host = ControllerHost::new(container, Click::new(|_, _, _| println!("Click")));
-
-    return controller_host;
 }
