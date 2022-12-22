871b719f40b503b904e49f1dcb6dd8db79e09a88
giacomo13 <giacomomaggio13@gmail.com>
2022-12-22 18:47:13 +0100

    callback version

--- src/view/editor/ui.rs
+++ src/view/editor/ui.rs
@@ -1,6 +1,34 @@
-use druid::widget::{TextBox, Widget};
+use druid::widget::{TextBox, Widget, WidgetExt};
+use druid::{
+    Env,
+    UpdateCtx,
+    widget::Controller
+};
 
 pub fn book_editor() -> impl Widget<String>{
-    let textbox = TextBox::new();
+    let textbox = build_root_widget();
     textbox
+}
+
+struct UpdateCallback();
+
+impl Controller<String, TextBox<String>> for UpdateCallback {
+    fn update(&mut self, 
+        child: &mut TextBox<String>, 
+        ctx: &mut UpdateCtx<'_, '_>, 
+        old_data: &String, 
+        data: &String, 
+        env: &Env
+    ) {
+        if old_data != data {
+            // the data has changed, you can call your function here
+            println!("{}", data);
+        }
+        // also inform the child that the data has changed
+        child.update(ctx, old_data, data, env)
+    }
+}
+
+fn build_root_widget() -> impl Widget<String> {
+    TextBox::new().controller(UpdateCallback())
 }
